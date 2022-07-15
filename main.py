7f37dca8aadcc107d0ed66cad3280036ca4b763e
Ricky <55766370+rsherget@users.noreply.github.com>
2022-07-15 12:35:14 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+import random
+
+
+
+width = 500
+height = 500
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((width, height), 0, 32)
+
+    while True:
+        red = 1
+
+
+main()
